98ba5f494ce6a21bdd8d11046d019aab0bfa6846
Kevin Beaumont <34346399+GossiTheDog@users.noreply.github.com>
2020-12-02 10:42:39 +0000

    Create patch.bat

---
+++ RcpEptMapper/patch.bat
@@ -0,0 +1,13 @@
+@echo off
+
+@rem -- Fix for RcpEptMapper and Dnscache service permission issue allowing elevation of privilege on Windows 7 and Server 2008/2008 R2
+@rem -- Make sure you have subinacl.exe (in this repositary) in same location as batch file.
+@rem -- 
+@rem -- Example deployment scenario: - drop files in SYSVOL file share, modify subinacl.exe location (e.g. \\domain\sysvol\subinacl.exe),
+@rem -- call from Computer login script in GPO (or psexec \\*)
+
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\RpcEptMapper\Performance
+subinacl.exe /subkeyreg HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\RpcEptMapper\Performance /revoke=Users
+
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Dnscache\Performance
+subinacl.exe /subkeyreg HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Dnscache\Performance /revoke=Users
